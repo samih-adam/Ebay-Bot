c1bb892530d54fb6b6694b49722688f178d1efdd
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-03 16:08:19 -0500

    gucci

--- hashim/ebay_store_check.py
+++ hashim/ebay_store_check.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+import selenium
+import os
 
+chromedriver = "/usr/local/bin/chromedriver"
+browser = webdriver.Chrome(chromedriver)
 url = "https://www.ebay.com/signin/"
-driver = webdriver.GoogleChrome()
+
 driver.implicitly_wait(9)
 driver.get(url)
 driver.maximize_window()
 time.sleep(3)
 
 user = driver.find_element(By.ID, "userid")
-user.send_keys("Add your email client")
+user.send_keys("abdels6375@gmail.com")
 time.sleep(1)
 password = driver.find_element(By.ID, "pass")
-password.send_keys("Add your password client")
-signIn = driver.find_element(By)
+password.send_keys("Middleeast98!")
+signIn = driver.find_element(By.ID, "sgnBT")
+signIn.click()
+
 
