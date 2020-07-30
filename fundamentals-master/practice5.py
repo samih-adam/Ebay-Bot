7af4a07c359a74465ad359b76c1fb0bdd4fe6f48
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-29 19:43:04 -0500

    commit

---
+++ fundamentals-master/practice5.py
@@ -0,0 +1,33 @@
+# # loops baby
+
+# #
+# # Example file for working with loops
+# #
+
+# # def main():
+# #     x = 0
+
+# #     # define a while loop
+
+# #     # while(x<5):
+# #     #     print(x)
+# #     #     x = x + 1
+    
+# #     for x in range (5, 10):
+# #         print(x)
+
+
+#     # define a for loop
+
+
+
+#     # use a for loop over a collection
+
+
+
+#     # use the break and continue statements
+#     for x in range(5,10):
+#         if (x == 7): break 
+#         print (x)
+
+#done 
